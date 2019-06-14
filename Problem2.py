5fd5c64251329548555c8227fbd9b04b886f6057
deepshekawat <51258313+deepshekawat@users.noreply.github.com>
2019-06-14 03:43:37 +0000

    Add files via upload

---
+++ Problem2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import datetime
+
+now = datetime.datetime.now()
+
+input("what is your name : ")
+x=int(input("enter your age :"))
+print("you will turn 95 years old in year : ",95-x+now.year)
